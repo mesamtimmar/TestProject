a63385f7570e1e9275e6f86731938cb7d2f93a83
Mesam Timmar <mesam.timmar@square63.com>
2016-06-16 14:51:05 +0500

    Commit the master branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,6 @@ http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [
 
  #before_filter :authenticate
 
-Hamzaaaa
 #def authenticate
 #render :text => 'before_filter'
 #end
